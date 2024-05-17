46b9bae448cb9a52ebc088cc099b7b92592bef67
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-05-17 18:42:14 +0200

    Add mediocry attempt to dynamic routes code mirror

---
+++ src/components/code-mirror.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import CodeMirror from "svelte-codemirror-editor";
+    import { json, jsonParseLinter } from "@codemirror/lang-json";
+    import {linter } from "@codemirror/lint";
+    import { page } from '$app/stores';
+
+    let extensions = [
+        linter(jsonParseLinter())
+    ]
+    
+    export let value: string;
+</script>
+
+<CodeMirror bind:value lang={json()} extensions={extensions} />
