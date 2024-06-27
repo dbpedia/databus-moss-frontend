dece1362c64caca7be403aa7b0013a7315c27ac6
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-06-27 16:02:34 +0200

    Create user page

---
+++ src/lib/components/button-group.svelte
@@ -0,0 +1,23 @@
+<scrip lang="ts">
+
+</scrip>
+
+<div class="header">
+    <div class="inline-flex rounded-md shadow-sm" role="group">
+        <button type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+            </svg>
+            Profile
+        </button>
+        <button type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
+            </svg>
+            Settings
+        </button>
+    </div>
+</div>
+
+<style>
+</style>
---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+	import ButtonGroup from "$lib/components/button-group.svelte";
+	import UserData from "$lib/components/user-data.svelte";
+    import { MossUtils } from "$lib/utils/moss-utils";
+    import {
+        Input,
+        Label,
+        Button,
+		Select,
+     } from "flowbite-svelte";
+
+    let errorMessage: string = "";
+
+</script>
+
+<div class="container">
+
+    <div class="header">
+        <h1>Create User</h1>
+        <ButtonGroup/>
+    </div>
+
+    <div class="body">
+        <UserData></UserData>
+    </div>
+
+    {#if errorMessage}
+        {errorMessage}
+    {/if}
+</div>
+
+<style>
+    .container {
+        align-items: center;
+        padding-bottom: 3px;
+        padding-top: 3px;
+        padding-left: 1em;
+        gap: 10px;
+        flex-wrap: nowrap;
+        margin-top: 20px;
+    }
+
+    .header {
+        align-items: center;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    .body {
+        background-color: burlywood;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+</style>
