efdfc9ec4125292e258824a1acb27ecfd0d6ff2d
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-06-28 15:02:15 +0200

    Add user table for api keys

---
+++ src/lib/components/new-user.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+        Checkbox,
+        TableSearch,
+        Input,
+		Button,
+    } from 'flowbite-svelte';
+    import { ClipboardCheckOutline } from "flowbite-svelte-icons";
+</script>
+
+<TableBodyRow>
+    <TableBodyCell>
+        <Input placeholder="Username"/>
+    </TableBodyCell>
+    <TableBodyCell>
+        <Input placeholder="User Sub"/>
+    </TableBodyCell>
+    <TableBodyCell>
+        <Input disabled placeholder="Generated API Key"/>
+    </TableBodyCell>
+    <TableBodyCell>
+            <Button>Add</Button>
+    </TableBodyCell>
+</TableBodyRow>
+
+<style>
+</style>
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
 	import ButtonGroup from "$lib/components/button-group.svelte";
+	import NewUser from "$lib/components/new-user.svelte";
 	import UserData from "$lib/components/user-data.svelte";
     import { MossUtils } from "$lib/utils/moss-utils";
     import {
@@ -8,21 +9,65 @@
         Label,
         Button,
 		Select,
+        Popover,
+        Heading,
      } from "flowbite-svelte";
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
+    import { ClipboardCheckOutline } from "flowbite-svelte-icons";
 
     let errorMessage: string = "";
+
+    const rowNames  = [
+        "Userbame",
+        "User Sub",
+        "API Key",
+    ];
+
+    let tabelItems = [
+        { key: 12345, sub: 'Toyota', name: 'ABC'},
+        { key: 67890, sub: 'Ford', name: 'CDE'},
+        { key: 54321, sub: 'Volvo', name: 'FGH'},
+        { key: 98765, sub: 'Saab', name: 'IJK'}
+    ];
 
 </script>
 
 <div class="container">
 
     <div class="header">
-        <h1>Create User</h1>
+        <Heading tag="h3" class="mb-2">User</Heading>
         <ButtonGroup/>
     </div>
-
+    <UserData></UserData>
     <div class="body">
-        <UserData></UserData>
+        <Heading tag="h3" class="mb-2">Create new API Key</Heading>
+        <Table class="table" shadow striped>
+            <TableHead>
+                {#each rowNames as name}
+                    <TableHeadCell>{name}</TableHeadCell>
+                {/each}
+            </TableHead>
+            <TableBody tableBodyClass="divide-y">
+                {#each tabelItems as item, index}
+                    <TableBodyRow>
+                        <TableBodyCell>{item.name}</TableBodyCell>
+                        <TableBodyCell>{item.sub}</TableBodyCell>
+                        <TableBodyCell>
+                            <div class="key-input">
+                                <Input id="key{index}" readonly value={item.key}/>
+                                <ClipboardCheckOutline id="b1" on:click={() => console.log("clicked")} class="clipboard"/>
+                                <!-- <Button id="b1">Default popover</Button> -->
+                                <Popover class="w-32 text-sm font-light " triggeredBy="#b1">Copy to clipboard</Popover>
+                            </div>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                                <Button>Revoke</Button>
+                        </TableBodyCell>
+                    </TableBodyRow>
+                {/each}
+            </TableBody>
+            <NewUser></NewUser>
+        </Table>
     </div>
 
     {#if errorMessage}
@@ -32,28 +77,40 @@
 
 <style>
     .container {
-        align-items: center;
-        padding-bottom: 3px;
         padding-top: 3px;
         padding-left: 1em;
+        padding-right: 1em;
         gap: 10px;
-        flex-wrap: nowrap;
-        margin-top: 20px;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
     }
 
     .header {
-        align-items: center;
         display: flex;
         flex-direction: row;
-        justify-content: space-around;
+        justify-content: space-between;
         margin-top: 1em;
         margin-bottom: 1em;
+        padding-left: 1em;
+        padding-right: 1em;
     }
 
     .body {
-        display: flex;
         flex-direction: row;
         justify-content: center;
+    }
+
+    .key-input {
+        justify-content: center;
+        display: flex;
+        align-items: center;
+    }
+
+    .table {
+        width: 100%;
+        flex-grow: 1;
     }
 
 </style>
