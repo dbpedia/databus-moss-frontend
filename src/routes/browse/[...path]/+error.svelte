7810077e248cee5a66e233fed840af4ce0cbabe3
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-07-11 11:09:12 +0200

    Add new error page for browse route

--- src/routes/browse/[...path]/+error.svelte
+++ src/routes/browse/[...path]/+error.svelte
@@ -1,10 +1,74 @@
 <script lang="ts">
-    import { A } from "flowbite-svelte";
+    import {
+        A,
+        Alert,
+    } from "flowbite-svelte";
+	import {
+         InfoCircleSolid,
+         HomeOutline
+    } from "flowbite-svelte-icons";
+
+    const msg = "Something went wrong. This route points to nowhere!";
+    const errorMsg = "404";
 </script>
 
-<h1>404: Not found</h1>
-<A href="/browse">Hütte</A>
+<div class="container">
+    <div class=icon>
+        <A href="/browse">
+            <HomeOutline class="w-14 h-14"/>
+        </A>
+    </div>
+    <A href="/browse">
+        <Alert class="full-width-alert">
+            <div class="info">
+                <InfoCircleSolid slot="icon" class="w-5 h-5"/>
+                <span class="leading-relaxed text-3xl">
+                    {errorMsg}
+                </span>
+            </div>
+            <p class="alert-message text-xl">
+                Oops!
+            </p>
+            <br>
+            <p class="alert-message">
+                {msg}
+            </p>
+        </Alert>
+    </A>
+</div>
 
 <style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 50vh;
+    }
 
+    .full-width-alert {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    .info {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+    .icon {
+        margin-bottom: 10px;
+    }
+
+    .alert-message {
+        display: flex;
+        justify-content: center;
+    }
 </style>
