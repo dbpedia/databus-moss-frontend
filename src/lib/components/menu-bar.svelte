8cf20ed00a9e5015d25950fa0c7f9f6040439019
holycrab13 <jan.forberg@hotmail.de>
2024-11-01 15:45:57 +0100

    added alpha notification

--- src/lib/components/menu-bar.svelte
+++ src/lib/components/menu-bar.svelte
@@ -16,9 +16,10 @@
         <img src="/moss.svg" class="me-3 h-6 sm:h-9" alt="Moss Logo" />
         <!-- <img src="/icon.svg" class="me-3 h-6 sm:h-9" alt="Moss Logo" /> -->
         <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">MOSS</span>
+        <span style="color: #aaa; margin-left: 0.5em">Alpha 0.1.1</span>
     </NavBrand>
     <NavHamburger  />
-    <NavUl >
+    <NavUl>
         <NavLi href="/">Home</NavLi>
         <NavLi href="/browse">Browse</NavLi>
         <NavLi href="/create-layer">Create Layer</NavLi>
